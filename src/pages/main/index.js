f5dfbe063da6cfac9cae6d2c06b2a5690cdc2db8
Bruno Marques <brunomandrade.br@gmail.com>
2020-03-23 17:19:31 -0300

    Adicionando navegaçao paginada

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -8,6 +8,8 @@ export default class Main extends Component {
     // Criando uma variável de estado, lembrando que state é sempre um objeto
     state = {
         products: [],
+        productInfo: {},
+        page: 1,
     }
 
     // Executa uma ação logo que o componente é exibido em tela.
@@ -16,22 +18,52 @@ export default class Main extends Component {
     }
 
     // Utilizando async => await para garantir que a promisse seja entregue a variavel definida para continuar.
-    loadProducts = async () => {
-        const response = await api.get('/products');
-        this.setState({ products: response.data.docs });
+    loadProducts = async (page = 1) => {
+
+        const response = await api.get(`/products?page=${page}`);
+
+        const { docs, ...productInfo } = response.data;
+
+        this.setState({ products: docs, productInfo, page });
+    }
+
+    prevPage = () => {
+        const { page, productInfo } = this.state;
+
+        if(page === 1 ) return;
+
+        const pageNumber = page -1;
+
+        this.loadProducts(pageNumber);
+
+     }
+
+    nextPage = () => {
+        const { page, productInfo } = this.state;
+
+        if (page === productInfo.pages) return;
+
+        const pageNumber = page + 1;
+
+        this.loadProducts(pageNumber);
+
     }
 
     render() {
-        const {products} = this.state;
+        const { products, page, productInfo } = this.state;
         return (
             <div className="product-list">
                 {products.map(product => (
                     <article key={product._id}>
                         <strong>{product.title}</strong>
                         <p>{product.description}</p>
-                        <a target="_blank" href={product.url}>Acessar</a>
+                        <a href={product.url}>Acessar</a>
                     </article>
                 ))}
+                <div className="actions">
+                    <button disabled={page===1} onClick={this.prevPage}>Anterior</button>
+                    <button disabled={page===productInfo.pages} onClick={this.nextPage}>Próxima</button>
+                </div>
             </div>
         )
     }
--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,7 @@
 import Axios from 'axios';
 
-const api = Axios.create({baseURL:'http://localhost:3001/api'});
+//const api = Axios.create({baseURL:'http://localhost:3001/api'});
+
+const api = Axios.create({ baseURL: 'https://rocketseat-node.herokuapp.com/api' });
 
 export default api;
